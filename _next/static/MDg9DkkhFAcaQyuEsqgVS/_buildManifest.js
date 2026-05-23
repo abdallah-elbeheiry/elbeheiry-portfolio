2168d66e7d24d67426f50f7b986fa7ce6f6f567d
Abdallah Elbeheiry <abdallahelbhary39@gmail.com>
2026-05-23 22:32:27 +0300

    Updates

--- _next/static/MDg9DkkhFAcaQyuEsqgVS/_buildManifest.js
+++ _next/static/MDg9DkkhFAcaQyuEsqgVS/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/elbeheiry-portfolio//_next/:path+",
-        "destination": "/elbeheiry-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
